2fbf7570a87f5eec5255ec4fae435a0aee07b094
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-07-09 18:19:31 +0500

    Arrow Functions
    Arrow functions were introduced in ES6.
    
    Arrow functions allow us to write shorter function syntax:
    
    let myFunction = (a, b) => a * b;

---
+++ arrowfun.js
@@ -0,0 +1,45 @@
+
+
+const calculateage = () => {
+
+}
+
+
+const calculateage2 = (a) => {
+    console.log(a)
+}
+
+let myFunction = (a, b) => a * b;
+document.getElementById("demo").innerHTML = myFunction(4, 5);
+
+
+
+hello = () => {
+    return "Hello World!";
+}
+
+
+
+//   <!DOCTYPE html>
+// <html>
+// <body>
+
+// <h1>JavaScript Functions</h1>
+// <h2>The Arrow Function</h2>
+
+// <p>This example shows the syntax of an Arrow Function, and how to use it.</p>
+
+// <p id="demo"></p>
+
+// <script>
+// let hello = "";
+
+// hello = () => {
+//   return "Hello World!";
+// }
+
+// document.getElementById("demo").innerHTML = hello();
+// </script>
+
+// </body>
+// </html>
