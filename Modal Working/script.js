ca2478a5f48fff8159e32eebcf151b90c74b4419
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-07-11 12:33:41 +0500

    Modal Refactored with Functions

--- Modal Working/script.js
+++ Modal Working/script.js
@@ -7,11 +7,22 @@ const btnCloseModal = document.querySelector(".close-modal");
 const btnOpenModal = document.querySelectorAll(".show-modal");
 //console.log(btnOpenModal);
 
-for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener("click", function () {
-    console.log("Button Clicked");
+const openModal = function () {
+  console.log("Button Clicked");
 
-    modal.classList.remove("hidden");
-    //console.log(modal.classList.classList());
-  });
+  modal.classList.remove("hidden");
+
+  overlay.classList.remove("hidden");
+};
+
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener("click", openModal);
 }
+
+const closeModal = function () {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
