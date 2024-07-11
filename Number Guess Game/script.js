0ac336c084da2dc191822020b003f37eebe6c852
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-07-11 10:29:58 +0500

    added query selector
    .value used for input field
    .text used for <p> field

--- Number Guess Game/script.js
+++ Number Guess Game/script.js
@@ -1,2 +1,8 @@
 "use strict";
-document.querySelector(".message");
+console.log(document.querySelector(".message").textContent);
+document.querySelector(".message").textContent = "Correct Number!🎁";
+
+document.querySelector(".number").textContent = 13;
+document.querySelector(".score").textContent = 2;
+
+document.querySelector(".guess").value = 29;
