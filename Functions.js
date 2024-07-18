6fc429f4e540b7fc75ca746d244500bb70c78d83
Shahzaib Shafiq <73356944+shahzaib-shafiq@users.noreply.github.com>
2024-07-18 12:56:01 +0500

    Functions Accepting Callback Functions

--- Functions.js
+++ Functions.js
@@ -63,15 +63,14 @@ const newPassport = function (person) {
 newPassport(jonas);
 checkIn(flight, jonas);
 
-/*
 ///////////////////////////////////////
 // Functions Accepting Callback Functions
 const oneWord = function (str) {
-  return str.replace(/ /g, '').toLowerCase();
+  return str.replace(/ /g, "").toLowerCase();
 };
 const upperFirstWord = function (str) {
-  const [first, ...others] = str.split(' ');
-  return [first.toUpperCase(), ...others].join(' ');
+  const [first, ...others] = str.split(" ");
+  return [first.toUpperCase(), ...others].join(" ");
 };
 // Higher-order function
 const transformer = function (str, fn) {
@@ -79,14 +78,14 @@ const transformer = function (str, fn) {
   console.log(`Transformed string: ${fn(str)}`);
   console.log(`Transformed by: ${fn.name}`);
 };
-transformer('JavaScript is the best!', upperFirstWord);
-transformer('JavaScript is the best!', oneWord);
+transformer("JavaScript is the best!", upperFirstWord);
+transformer("JavaScript is the best!", oneWord);
 // JS uses callbacks all the time
 const high5 = function () {
-  console.log('👋');
+  console.log("👋");
 };
-document.body.addEventListener('click', high5);
-['Jonas', 'Martha', 'Adam'].forEach(high5);
+document.body.addEventListener("click", high5);
+["Jonas", "Martha", "Adam"].forEach(high5);
 ///////////////////////////////////////
 // Functions Returning Functions
 const greet = function (greeting) {
@@ -94,18 +93,18 @@ const greet = function (greeting) {
     console.log(`${greeting} ${name}`);
   };
 };
-const greeterHey = greet('Hey');
-greeterHey('Jonas');
-greeterHey('Steven');
-greet('Hello')('Jonas');
+const greeterHey = greet("Hey");
+greeterHey("Jonas");
+greeterHey("Steven");
+greet("Hello")("Jonas");
 // Challenge
-const greetArr = greeting => name => console.log(`${greeting} ${name}`);
-greetArr('Hi')('Jonas');
+const greetArr = (greeting) => (name) => console.log(`${greeting} ${name}`);
+greetArr("Hi")("Jonas");
 ///////////////////////////////////////
 // The call and apply Methods
 const lufthansa = {
-  airline: 'Lufthansa',
-  iataCode: 'LH',
+  airline: "Lufthansa",
+  iataCode: "LH",
   bookings: [],
   // book: function() {}
   book(flightNum, name) {
@@ -115,33 +114,37 @@ const lufthansa = {
     this.bookings.push({ flight: `${this.iataCode}${flightNum}`, name });
   },
 };
-lufthansa.book(239, 'Jonas Schmedtmann');
-lufthansa.book(635, 'John Smith');
+lufthansa.book(239, "Jonas Schmedtmann");
+lufthansa.book(635, "John Smith");
 const eurowings = {
-  airline: 'Eurowings',
-  iataCode: 'EW',
+  airline: "Eurowings",
+  iataCode: "EW",
   bookings: [],
 };
 const book = lufthansa.book;
 // Does NOT work
 // book(23, 'Sarah Williams');
 // Call method
-book.call(eurowings, 23, 'Sarah Williams');
+book.call(eurowings, 23, "Sarah Williams");
 console.log(eurowings);
-book.call(lufthansa, 239, 'Mary Cooper');
+book.call(lufthansa, 239, "Mary Cooper");
 console.log(lufthansa);
 const swiss = {
-  airline: 'Swiss Air Lines',
-  iataCode: 'LX',
+  airline: "Swiss Air Lines",
+  iataCode: "LX",
   bookings: [],
 };
-book.call(swiss, 583, 'Mary Cooper');
+book.call(swiss, 583, "Mary Cooper");
+
 // Apply method
-const flightData = [583, 'George Cooper'];
+const flightData = [583, "George Cooper"];
 book.apply(swiss, flightData);
 console.log(swiss);
 book.call(swiss, ...flightData);
 ///////////////////////////////////////
+
+/*
+
 // The bind Method
 // book.call(eurowings, 23, 'Sarah Williams');
 const bookEW = book.bind(eurowings);
